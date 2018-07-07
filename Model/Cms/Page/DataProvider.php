74270a9537f1d4b299566ee3ba0b4f1ed8ccf434
Ian Bastos <ian-bastos@live.com>
2018-07-08 00:29:02 +0100

    fixed data provider for cms pages issue with underlying data

--- Model/Cms/Page/DataProvider.php
+++ Model/Cms/Page/DataProvider.php
@@ -24,32 +24,42 @@ class DataProvider extends \Magento\Cms\Model\Page\DataProvider
         if (isset($this->loadedData)) {
             return $this->loadedData;
         }
+
         $items = $this->collection->getItems();
+
         /** @var $page \Magento\Cms\Model\Page */
         foreach ($items as $page) {
             $this->loadedData[$page->getId()] = $page->getData();
         }
 
         $data = $this->dataPersistor->get('cms_page');
-        $page = $this->collection->getNewEmptyItem();
 
         if (!empty($data)) {
+            $page = $this->collection->getNewEmptyItem();
             $page->setData($data);
             $this->loadedData[$page->getId()] = $page->getData();
             $this->dataPersistor->clear('cms_page');
         }
 
-        if(!empty($this->loadedData[$page->getId()]['page_header_image'])){
-            $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
-            $storeManager = $objectManager->get('Magento\Store\Model\StoreManagerInterface');
-            $currentStore = $storeManager->getStore();
-            $media_url=$currentStore->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_MEDIA);
+        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+        $storeManager = $objectManager->get('Magento\Store\Model\StoreManagerInterface');
+        $currentStore = $storeManager->getStore();
+        $media_url=$currentStore->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_MEDIA);
 
+        if(!empty($this->loadedData[$page->getId()]['page_header_image'])){
             $image_name=$this->loadedData[$page->getId()]['page_header_image'];
             unset($this->loadedData[$page->getId()]['page_header_image']);
             $this->loadedData[$page->getId()]['page_header_image'][0]['name']=$image_name;
             $this->loadedData[$page->getId()]['page_header_image'][0]['url']=$media_url."cms/headerimages/".$image_name;
         }
+
+        if(!empty($this->loadedData[$page->getId()]['page_header_background_image'])){
+            $image_name=$this->loadedData[$page->getId()]['page_header_background_image'];
+            unset($this->loadedData[$page->getId()]['page_header_background_image']);
+            $this->loadedData[$page->getId()]['page_header_background_image'][0]['name']=$image_name;
+            $this->loadedData[$page->getId()]['page_header_background_image'][0]['url']=$media_url."cms/headerimages/".$image_name;
+        }
+
         return $this->loadedData;
     }
 }
